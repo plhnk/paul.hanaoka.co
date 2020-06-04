5509687148c1da9c0a342ef85fdbbecab130cb4e
plhnk <paul.hanaoka@gmail.com>
2020-06-04 10:41:27 -0700

    make footer content responsive

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,36 +1,31 @@
 /** @jsx jsx */
 
-import { jsx, Text } from 'theme-ui'
+import { jsx, Text } from 'theme-ui';
 import Wrapper from '../components/wrapper';
 import Link from './link';
+import { useMediaQuery } from 'react-responsive';
 
-export default () => (
-  <footer
-    sx={{
-      mb:[5,null, 4]
-    }}
-  >
-    <Wrapper>
-      <Text sx={{color: 'neutral1'}}>
-        Financed by{' '}
-        <Link to="https://liferay.com">
-          Liferay
-        </Link>
-        , powered by{' '}
-        <Link to="https://gatsbyjs.com">
-          Gatsby
-        </Link>
-        , hosted by{' '}
-        <Link to="https://netlify.com">
-          Netlify
-        </Link>
-        .
-      </Text>
-      <Text 
-        sx={{color: 'neutral3'}}
-      >
-        Copyright © {new Date().getFullYear()} Paul Hanaoka.
-      </Text>
-    </Wrapper>
-  </footer>
-);
+export default () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  return (
+    <footer
+      sx={{
+        mb: [5, null, 4],
+      }}
+    >
+      <Wrapper>
+        <Text sx={{ color: 'neutral1' }}>
+          {isMobile ? '💵' : 'Financed by'} {' '}
+          <Link to="https://liferay.com">Liferay</Link>,{' '}
+          {isMobile ? '⚡️' : 'powered by'} {' '}
+          <Link to="https://gatsbyjs.com">Gatsby</Link>,{' '}
+          {isMobile ? '🏡' : 'hosted by'} {' '}
+          <Link to="https://netlify.com">Netlify</Link>.
+        </Text>
+        <Text sx={{ color: 'neutral3' }}>
+          Copyright © {new Date().getFullYear()} Paul Hanaoka.
+        </Text>
+      </Wrapper>
+    </footer>
+  );
+};
