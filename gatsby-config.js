465c0ad76023cc0a1850f91cef1a7094e4ab9849
plhnk <paul.hanaoka@gmail.com>
2020-06-06 12:49:02 -0700

    add and configure gatsby-source-unsplash

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+
 module.exports = {
   plugins: [
     {
@@ -11,6 +16,16 @@ module.exports = {
       options: {
         name: `posts`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-unsplash`,
+      options: {
+        secretKey: process.env.UNSPLASH_SECRET_KEY,
+        appId: process.env.UNSPLASH_ACCESS_KEY,
+        collections: [`10621197`],
+        // optional: will only get page 1, so increase this count to include > 10 photos
+        perPage: `100`,
       },
     },
     {
