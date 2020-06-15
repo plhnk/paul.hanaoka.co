2f1870b3c8e60c09b9d267b2058b0b052ba70df8
plhnk <paul.hanaoka@gmail.com>
2020-06-15 00:53:58 -0700

    start to add time sensitive theme option

--- src/components/themeToggle.js
+++ src/components/themeToggle.js
@@ -1,9 +1,10 @@
 /** @jsx jsx */
-import { jsx, useColorMode, IconButton } from 'theme-ui'
+import { jsx, useColorMode, IconButton } from 'theme-ui';
 import Icon from '../components/icon';
 
-export default props => {
-  const [ mode, setMode ] = useColorMode()
+export default (props) => {
+  const [mode, setMode] = useColorMode();
+
   return (
     <IconButton
       aria-label="Theme toggle"
@@ -16,4 +17,17 @@ export default props => {
       <Icon name={`${mode === 'dark' ? 'lightMode' : 'darkMode'}`} />
     </IconButton>
   );
-}
+};
+
+// const [mode, setMode] = useColorMode();
+
+// const timeFormat = 'HH'; // set time format to something easy
+// const time = moment();
+// const dayStart = moment('08', timeFormat);
+// const dayEnd = moment('20', timeFormat);
+
+// const timeSensitiveTheme = time.isBetween(dayStart, dayEnd)
+//   ? setMode('light')
+//   : setMode('dark');
+// // time.isBetween(dayStart, dayEnd) ? setMode('dark') : setMode('light');
+// console.log(time);
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,6 +1,5 @@
 const unsplashReferral = '?utm_source=https://paul.hanaoka.co&utm_medium=referral';
 
-
 export function addReferral(input) {
   const addReferral = input + unsplashReferral;
   return addReferral;
