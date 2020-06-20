3fa222a11c8b31cc0f56c4f8f472bf1b1b638efd
plhnk <paul.hanaoka@gmail.com>
2020-06-20 01:40:06 -0700

    style post card

--- src/components/post.js
+++ src/components/post.js
@@ -1,14 +1,7 @@
 /** @jsx jsx */
 
-import {
-  jsx,
-  Text,
-  Flex,
-  Box,
-  Styled
-} from 'theme-ui';
+import { jsx, Text, Flex, Box, Styled, Card } from 'theme-ui';
 import Link from './link';
-
 
 export default ({
   id,
@@ -18,22 +11,29 @@ export default ({
   postTitle,
   postExcerpt,
   postDate,
-  readTime
+  readTime,
 }) => {
-  
-  const H2 = Styled.h2
+  const H2 = Styled.h2;
   return (
-    <Link sortDate={sortDate} to={postSlug} sx={{ textDecoration: 'none' }}>
-      <Box
-        sx={{
-          p: [null, 3, null],
-          mt: 6,
-          maxWidth: '70ch',
-        }}
-        key={id}
-      >
-        <Text as='h3' sx={{fontSize:2, color: 'neutral2', variant: 'text.smallcaps'}}>{superTitle}</Text>
-        <H2 sx={{ color: 'neutral2' }}>{postTitle}</H2>
+    <Link
+      sortDate={sortDate}
+      to={postSlug}
+      sx={{
+        textDecoration: 'none',
+        mt: 6,
+        '&:hover': { backgroundColor: 'neutral9' },
+        p: [null, 3, null],
+        maxWidth: '70ch',
+      }}
+      key={id}
+    >
+        <Text
+          as="h3"
+          sx={{ fontSize: 2, color: 'neutral2', variant: 'text.smallcaps' }}
+        >
+          {superTitle}
+        </Text>
+        <H2 sx={{ mt:1, color: 'neutral2' }}>{postTitle}</H2>
         <Text sx={{ color: 'neutral3' }}>{postExcerpt}</Text>
         <Flex
           sx={{ mt: 3, justifyContent: 'space-between', alignItems: 'center' }}
@@ -43,7 +43,6 @@ export default ({
           </Text>
           <Text>{postDate}</Text>
         </Flex>
-      </Box>
     </Link>
   );
 };
