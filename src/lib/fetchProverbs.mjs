48d90210456eea5703cc8d8d59ebbbd9a3384339
plhnk <paul.hanaoka@gmail.com>
2024-04-23 11:31:41 -0700

    get daily proverb working

---
+++ src/lib/fetchProverbs.mjs
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+import passages from './proverbsList.js';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../../.env.local' });
+
+const API_KEY = process.env.ESV_API_KEY;
+
+let allData = {};
+
+const fetches = passages.map((passage, index) => {
+  const params = new URLSearchParams({
+    'include-passage-references': 'false',
+    'include-verse-numbers': 'false',
+    'include-short-copyright': 'false',
+    'include-footnotes': 'false',
+  });
+  const API_URL = `https://api.esv.org/v3/passage/text/?q=${passage}&${params.toString()}`;
+  
+  return fetch(API_URL, {
+    method: 'GET',
+    headers: {
+      Authorization: `Token ${API_KEY}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      allData[index] = data;
+    })
+    .catch((error) => console.error('Error:', error));
+});
+
+Promise.all(fetches).then(() => {
+  fs.mkdirSync('./data', { recursive: true });
+  fs.writeFileSync(`./data/proverbs.json`, JSON.stringify(allData, null, 2));
+});
