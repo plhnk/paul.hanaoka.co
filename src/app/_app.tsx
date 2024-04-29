41f4b0865443f0db100b3898820ce2b01c77e929
plhnk <paul.hanaoka@gmail.com>
2024-04-29 13:19:31 -0700

    add fathom analytics

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import Layout from "./layout";
+import * as Fathom from 'fathom-client';
 
 interface AppProps {
   Component: React.ComponentType<any>;
@@ -6,6 +8,15 @@ interface AppProps {
 }
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      Fathom.load('ERQBAFPH', {
+        includedDomains: ['paul.hanaoka.co']
+      });
+      Fathom.trackPageview();
+    }
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
