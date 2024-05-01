8de24b5fd42d9627d2e18d9a502415f885bf5b47
plhnk <paul.hanaoka@gmail.com>
2024-05-01 14:33:16 -0700

    polish mobile layout sidebar / photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="sidebar">
         <Calendar className="col-span-2" />
         <Weather className="col-span-2" />
-        <Photos className='col-span-4 sm:col-span-2 h-[80dvh] mt-2 sm:mt-0 sm:h-full' />
+        <Photos className='col-span-4 sm:col-span-2 h-[80vh] mt-4 sm:mt-0 sm:h-full' />
         {/* <Proverbs/> */}
       </div>
     </>
@@ -24,3 +24,5 @@ export default function Home() {
 // TODO --> extract grids into tailwind for easier re-use (main, sidebar)
 
 // grid sticky top-0 items-stretch sm:flex sm:flex-col grid-cols-2 row-span-2 sm:grid-cols-1 gap-4 sm:gap-8 mb-8
+
+// TODO --> photos height is wonky on desktop
