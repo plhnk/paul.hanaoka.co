414242e7915bb0c34354893331ea4b2089e73dc7
plhnk <paul.hanaoka@gmail.com>
2024-05-06 23:52:49 -0700

    rough out directional arrows

--- src/components/recommends.tsx
+++ src/components/recommends.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 import { RecommendsProps } from '@/lib/utilities/types';
 import Link from '../components/ui/link';
 import recommendsData from '@/lib/data/recommends.json';
@@ -12,6 +12,7 @@ import {
   TooltipTrigger,
 } from './ui/tooltip';
 import Image from 'next/image';
+import { ArrowDown, ArrowUp } from 'lucide-react';
 
 const Recommends: React.FC<{ className?: string }> = ({ className }) => {
   const [recommends, setRecommends] = useState<RecommendsProps[]>([]);
@@ -37,17 +38,66 @@ const Recommends: React.FC<{ className?: string }> = ({ className }) => {
     window.scrollTo({ top, behavior: 'smooth' });
   };
 
+  // Create a state to store the direction of each arrow
+  const [arrowDirections, setArrowDirections] = useState<
+    Record<string, 'up' | 'down' | 'none'>
+  >({});
+
+  // Create categoryRefs
+  const categoryRefs = Object.keys(groupedRecommends).reduce(
+    (refs, category) => {
+      refs[category] = React.createRef();
+      return refs;
+    },
+    {} as Record<string, React.RefObject<HTMLDivElement>>
+  );
+
+  // Update arrowDirections when the scroll position changes
+  useEffect(() => {
+    const handleScroll = () => {
+      const newArrowDirections = Object.entries(categoryRefs).reduce(
+        (directions, [category, ref]) => {
+          if (ref.current) {
+            const rect = ref.current.getBoundingClientRect();
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+              directions[category] = 'none';
+            } else if (rect.top > window.innerHeight) {
+              directions[category] = 'down';
+            } else {
+              directions[category] = 'up';
+            }
+          }
+          return directions;
+        },
+        {} as Record<string, 'up' | 'down' | 'none'>
+      );
+      setArrowDirections(newArrowDirections);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [categoryRefs]);
+
   return (
     <div className={`flex flex-col gap-y-32 my-48 main-content ${className}`}>
-      <div className="bg-background/20 backdrop-blur-lg py-4 sticky top-11 z-30 w-full flex justify-between">
+      <div className="-ml-6 px-9 py-2 rounded-xl bg-background/20 backdrop-blur-lg sticky top-11 z-30 w-full col-span-4 flex">
         {Object.keys(groupedRecommends).map((category) => (
-          <button onClick={() => scrollTo(category)} key={category}>
+          <button
+            className="p-3 mr-8 inline"
+            onClick={() => scrollTo(category)}
+            key={category}
+          >
             {category}
+            {arrowDirections[category] === 'up' ? (
+              <ArrowUp />
+            ) : arrowDirections[category] === 'down' ? (
+              <ArrowDown />
+            ) : null}
           </button>
         ))}
       </div>
       {Object.entries(groupedRecommends).map(([category, recommends]) => (
-        <div key={category} id={category}>
+        <div key={category} id={category} ref={categoryRefs[category]}>
           <ul className="my-16">
             {recommends.map((recommend, index) => (
               <li className="flex relative my-12" key={index}>
@@ -94,7 +144,9 @@ const Recommends: React.FC<{ className?: string }> = ({ className }) => {
                     <div
                       className="absolute w-[200%] h-[200%] bg-blend-hard-light bg-cover bg-left-bottom bg-no-repeat bg-background pointer-events-none inset-0 blur-3xl z-0 from-background/80 to-background/30 -rotate-12"
                       style={{
-                        backgroundImage: `linear-gradient(to top right, var(--tw-gradient-stops)), url(${'/images/' + recommend.icon})`,
+                        backgroundImage: `linear-gradient(to top right, var(--tw-gradient-stops)), url(${
+                          '/images/' + recommend.icon
+                        })`,
                       }}
                     />
                   </CardContent>
