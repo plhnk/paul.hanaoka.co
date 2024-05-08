f5ed790c513a034b0357770d87349f2d5e161600
plhnk <paul.hanaoka@gmail.com>
2024-05-08 14:59:42 -0700

    abstract onPageNav, add to About

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,19 +1,44 @@
 'use client';
-import React from 'react';
-
+import React, { useRef } from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import MdxLayout from '../../components/mdx-layout';
 import Content from './content.mdx';
 import Colophon from './colophon.mdx';
 import CV from './cv.mdx';
+import Bio from './bio.mdx';
+import OnPageNav from '../../components/ui/on-page-nav';
 
 const About: React.FC = () => {
+  // const contentRef = useRef<HTMLDivElement>(null);
+  const colophonRef = useRef<HTMLDivElement>(null);
+  const cvRef = useRef<HTMLDivElement>(null);
+  const bioRef = useRef<HTMLDivElement>(null);
+
+  const categories = ['bio','colophon', 'cv'];
+
+  const scrollTo = (id: string) => {
+    const element = document.getElementById(id);
+    if (!element) return;
+    const top = element.getBoundingClientRect().top + window.pageYOffset - 200;
+    window.scrollTo({ top, behavior: 'smooth' });
+  };
+
   return (
     <MDXProvider>
       <MdxLayout>
-        <Content />
-        <Colophon />
-        {/* <CV /> */}
+        {/* <div id="content" ref={contentRef}> */}
+          <Content />
+        <OnPageNav categories={categories} scrollTo={scrollTo} />
+        {/* </div> */}
+        <div id="bio" ref={bioRef}>
+          <Bio />
+        </div>
+        <div id="colophon" ref={colophonRef}>
+          <Colophon />
+        </div>
+        <div id="cv" ref={cvRef}>
+          <CV />
+        </div>
       </MdxLayout>
     </MDXProvider>
   );
--- src/components/recommends.tsx
+++ src/components/recommends.tsx
@@ -4,6 +4,7 @@ import { RecommendsProps } from '@/lib/utilities/types';
 import Link from '../components/ui/link';
 import recommendsData from '@/lib/data/recommends.json';
 import { Card, CardHeader, CardTitle, CardContent } from './ui/card';
+import OnPageNav from './ui/on-page-nav';
 
 import {
   Tooltip,
@@ -12,8 +13,8 @@ import {
   TooltipTrigger,
 } from './ui/tooltip';
 import Image from 'next/image';
-import { ArrowDown, ArrowUp, ArrowRight } from 'lucide-react';
-import ProgressiveBlur from './ui/progressiveblur';
+// import { ArrowDown, ArrowUp, ArrowRight } from 'lucide-react';
+// import ProgressiveBlur from './ui/progressiveblur';
 
 const Recommends: React.FC<{ className?: string }> = ({ className }) => {
   const [recommends, setRecommends] = useState<RecommendsProps[]>([]);
@@ -40,10 +41,10 @@ const Recommends: React.FC<{ className?: string }> = ({ className }) => {
     window.scrollTo({ top, behavior: 'smooth' });
   };
 
-  // Create a state to store the direction of each arrow
-  const [arrowDirections, setArrowDirections] = useState<
-    Record<string, 'up' | 'down' | 'none'>
-  >({});
+  // // Create a state to store the direction of each arrow
+  // const [arrowDirections, setArrowDirections] = useState<
+  //   Record<string, 'up' | 'down' | 'none'>
+  // >({});
 
   // Create categoryRefs
   const categoryRefs = Object.keys(groupedRecommends).reduce(
@@ -55,93 +56,64 @@ const Recommends: React.FC<{ className?: string }> = ({ className }) => {
   );
 
   // Update arrowDirections when the scroll position changes
-  useEffect(() => {
-    const handleScroll = () => {
-      const newArrowDirections = Object.entries(categoryRefs).reduce(
-        (directions, [category, ref]) => {
-          if (ref.current) {
-            const rect = ref.current.getBoundingClientRect();
-            const middleOfElement = rect.top + rect.height / 2;
-            const middleOfViewport = window.innerHeight / 2;
-            if (middleOfElement >= 0 && middleOfElement <= window.innerHeight) {
-              directions[category] = 'none';
-            } else if (middleOfElement > middleOfViewport) {
-              directions[category] = 'down';
-            } else {
-              directions[category] = 'up';
-            }
-          }
-          return directions;
-        },
-        {} as Record<string, 'up' | 'down' | 'none'>
-      );
-      setArrowDirections(newArrowDirections);
-    };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const newArrowDirections = Object.entries(categoryRefs).reduce(
+  //       (directions, [category, ref]) => {
+  //         if (ref.current) {
+  //           const rect = ref.current.getBoundingClientRect();
+  //           const middleOfElement = rect.top + rect.height / 2;
+  //           const middleOfViewport = window.innerHeight / 2;
+  //           if (middleOfElement >= 0 && middleOfElement <= window.innerHeight) {
+  //             directions[category] = 'none';
+  //           } else if (middleOfElement > middleOfViewport) {
+  //             directions[category] = 'down';
+  //           } else {
+  //             directions[category] = 'up';
+  //           }
+  //         }
+  //         return directions;
+  //       },
+  //       {} as Record<string, 'up' | 'down' | 'none'>
+  //     );
+  //     setArrowDirections(newArrowDirections);
+  //   };
 
-    // Nice Horizontal Arrow --> disappears when you get to end
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [categoryRefs]);
-  const [arrowOpacity, setArrowOpacity] = useState(1);
-  const [arrowRotate, setArrowRotate] = useState(0); 
+  //   // Nice Horizontal Arrow --> disappears when you get to end
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, [categoryRefs]);
+  // const [arrowOpacity, setArrowOpacity] = useState(1);
+  // const [arrowRotate, setArrowRotate] = useState(0); 
 
-  useEffect(() => {
-    const parentDiv = document.getElementById('onPageNav');
-    if (!parentDiv) return;
+  // useEffect(() => {
+  //   const parentDiv = document.getElementById('onPageNav');
+  //   if (!parentDiv) return;
 
-    const handleScroll = () => {
-      // Calculate the maximum scroll position
-      const maxScrollLeft = parentDiv.scrollWidth - parentDiv.clientWidth;
-      // Calculate the opacity based on the current scroll position and the maximum scroll position
-      const opacity = parentDiv.scrollLeft < maxScrollLeft ? 1 - parentDiv.scrollLeft / maxScrollLeft : 1;
-      const rotate = parentDiv.scrollLeft < maxScrollLeft ? 0 : 180;
-      // Set the opacity state variable to the calculated opacity
-      setArrowOpacity(opacity);
-      setArrowRotate(rotate);
-    };
+  //   const handleScroll = () => {
+  //     // Calculate the maximum scroll position
+  //     const maxScrollLeft = parentDiv.scrollWidth - parentDiv.clientWidth;
+  //     // Calculate the opacity based on the current scroll position and the maximum scroll position
+  //     const opacity = parentDiv.scrollLeft < maxScrollLeft ? 1 - parentDiv.scrollLeft / maxScrollLeft : 1;
+  //     const rotate = parentDiv.scrollLeft < maxScrollLeft ? 0 : 180;
+  //     // Set the opacity state variable to the calculated opacity
+  //     setArrowOpacity(opacity);
+  //     setArrowRotate(rotate);
+  //   };
 
-    parentDiv.addEventListener('scroll', handleScroll);
-    return () => parentDiv.removeEventListener('scroll', handleScroll);
-  }, []);
+  //   parentDiv.addEventListener('scroll', handleScroll);
+  //   return () => parentDiv.removeEventListener('scroll', handleScroll);
+  // }, []);
 
-  const iconStyles = {
-    className: 'invisible group-hover:visible opacity-80 absolute -right-4',
-    stroke: 'currentColor',
-  };
+  // const iconStyles = {
+  //   className: 'invisible group-hover:visible opacity-80 absolute -right-4',
+  //   stroke: 'currentColor',
+  // };
 
   return (
     <div className={`relative flex flex-col mt-40 2xl:mt-0 main-content ${className} `}>
-      <div
-        className={
-          '-ml-3 pl-4 sm:-ml-6 sm:px-9 2xl:px-4 py-2 rounded-xl sticky top-0 sm:top-10 z-30 w-[calc(100% + .75rem)] sm:col-span-4 flex overflow-x-scroll'
-        }
-        id="onPageNav"
-      >
-        {/* overflow-visible  */}
-        {/*  overflow-x-scroll overflow-y-clip */}
-        {Object.keys(groupedRecommends).map((category) => (
-          <button
-            className="group text-nowrap p-3 mr-8 flex relative"
-            onClick={() => scrollTo(category)}
-            key={category}
-          >
-            {category}
-            {arrowDirections[category] === 'up' ? (
-              <ArrowUp {...iconStyles} />
-            ) : arrowDirections[category] === 'down' ? (
-              <ArrowDown {...iconStyles} />
-            ) : null}
-          </button>
-        ))}
-        <div className="sm:hidden sticky rounded-md right-0 z-10 backdrop-blur-md my-auto p-3 flex">
-          <ArrowRight
-            size={24}
-            className="center-self-center opacity-80 transition-transform ease-in-out"
-            style={{ opacity: arrowOpacity, transform: `rotate(${arrowRotate}deg)` }}
-          />
-        </div>
-      </div>
-      <ProgressiveBlur className="block sticky -ml-4 sm:-ml-80 -mt-10 z-20 top-0 h-32 sm:h-64 w-dvw max-w-4xl 2xl:-ml-32 rotate-180" />
+      <OnPageNav categories={Object.keys(groupedRecommends)} scrollTo={scrollTo}/>
+      
       {Object.entries(groupedRecommends).map(([category, recommends]) => (
         <div
           className="my-16 sm:my-32 2xl:my-20"
@@ -247,3 +219,35 @@ export default Recommends;
 // TODO --> style current onPage items for mobile
 // TODO --> styles for the diff tags
 // TODO --> link tags? lots more work there
+
+// <div
+//         className={
+//           '-ml-3 pl-4 sm:-ml-6 sm:px-9 2xl:px-4 py-2 rounded-xl sticky top-0 sm:top-10 z-30 w-[calc(100% + .75rem)] sm:col-span-4 flex overflow-x-scroll'
+//         }
+//         id="onPageNav"
+//       >
+//         {/* overflow-visible  */}
+//         {/*  overflow-x-scroll overflow-y-clip */}
+//         {Object.keys(groupedRecommends).map((category) => (
+//           <button
+//             className="group text-nowrap p-3 mr-8 flex relative"
+//             onClick={() => scrollTo(category)}
+//             key={category}
+//           >
+//             {category}
+//             {arrowDirections[category] === 'up' ? (
+//               <ArrowUp {...iconStyles} />
+//             ) : arrowDirections[category] === 'down' ? (
+//               <ArrowDown {...iconStyles} />
+//             ) : null}
+//           </button>
+//         ))}
+//         <div className="sm:hidden sticky rounded-md right-0 z-10 backdrop-blur-md my-auto p-3 flex">
+//           <ArrowRight
+//             size={24}
+//             className="center-self-center opacity-80 transition-transform ease-in-out"
+//             style={{ opacity: arrowOpacity, transform: `rotate(${arrowRotate}deg)` }}
+//           />
+//         </div>
+//       </div>
+//       <ProgressiveBlur className="block sticky -ml-4 sm:-ml-80 -mt-10 z-20 top-0 h-32 sm:h-64 w-dvw max-w-4xl 2xl:-ml-32 rotate-180" />
