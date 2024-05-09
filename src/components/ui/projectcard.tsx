1df7aba61d655e55ac1b747e385002cb1f61339c
plhnk <paul.hanaoka@gmail.com>
2024-05-09 00:47:56 -0700

    try tilt shift effect

--- src/components/ui/projectcard.tsx
+++ src/components/ui/projectcard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Image from 'next/image';
+import ProgressiveBlur from './progressiveblur';
 
 interface ProjectCardProps {
   label: string;
@@ -25,14 +26,16 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
             width={900}
             height={600}
           />
+          {/* <ProgressiveBlur className="group-hover:invisible absolute -bottom-48 -right-48 rotate-[-45deg] w-[30rem] h-[30rem]" />
+          <ProgressiveBlur className="group-hover:invisible absolute -top-48 -left-48 rotate-[135deg] w-[30rem] h-[30rem]" /> */}
         </div>
         <div className="shadow" />
       </CardContent>
-      <CardHeader className='px-0'>
+      <CardHeader className="px-0">
         <CardTitle className="normal-case mb-2 tracking-normal *:block *:font-normal">
-          <span className='text-text/40 uppercase  leading-loose'>{label}</span>
-          <span className='text-text text-base'>{title}</span>
-          <span className='text-text/60 text-base'>{subtitle}</span>
+          <span className="text-text/40 uppercase  leading-loose">{label}</span>
+          <span className="text-text text-base">{title}</span>
+          <span className="text-text/60 text-base">{subtitle}</span>
         </CardTitle>
       </CardHeader>
     </Card>
