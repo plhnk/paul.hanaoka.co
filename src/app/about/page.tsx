fa3a3da972cd1bb95f65d5cb2c0fe7dda28795aa
plhnk <paul.hanaoka@gmail.com>
2024-05-08 23:06:26 -0700

    polish

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,13 +6,15 @@ import Content from './content.mdx';
 import Colophon from './colophon.mdx';
 import CV from './cv.mdx';
 import Bio from './bio.mdx';
+import HowItsMade from './how-i-built-this.mdx'
 import OnPageNav from '../../components/ui/on-page-nav';
 
 const About: React.FC = () => {
   const sections = [
     { id: 'Bio', component: Bio},
-    { id: 'Colophon', component: Colophon},
+    { id: 'How It\'s Made', component: HowItsMade},
     { id: 'Experience', component: CV},
+    { id: 'Colophon', component: Colophon},
   ];
 
   const categoryRefs = sections.reduce((refs, section) => {
@@ -24,7 +26,8 @@ const About: React.FC = () => {
     <MDXProvider>
       <MdxLayout>
         <Content />
-        <OnPageNav className='pl-1 -ml-4 sm:px-3 2xl:px-2' categories={Object.keys(categoryRefs)} scrollOffset={200} />
+        <div className='mb-20'/>
+        <OnPageNav className='pl-1 -ml-4 sm:px-3 2xl:px-2' categories={Object.keys(categoryRefs)} scrollOffset={240} />
         {sections.map(({ id, component: Component }) => (
           <div className="mb-80" id={id} key={id} ref={categoryRefs[id]}>
             <Component />
