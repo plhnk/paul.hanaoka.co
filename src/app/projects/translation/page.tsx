efa7cfa5977ba2e27d15201371310437ecfe9dc8
plhnk <paul.hanaoka@gmail.com>
2024-05-14 17:11:16 -0700

    rework Projects, get away from Figma prototypes

--- src/app/projects/translation/page.tsx
+++ src/app/projects/translation/page.tsx
@@ -1,20 +1,14 @@
 'use client';
 import React from 'react';
-import { MDXProvider } from '@mdx-js/react';
 import MdxLayout from '../../../components/mdx-layout';
 import Content from './content.mdx';
-import ProjectsComponent from '@/components/projects';
 
-// const Projects: React.FC = () => {
 export default function Projects() {
   return (
-    <div className="main-content">
-      <MDXProvider>
+    <div className="col-start-3 col-span-7">
         <MdxLayout>
           <Content />
         </MdxLayout>
-      </MDXProvider>
-      {/* <ProjectsComponent /> */}
     </div>
   );
 }
