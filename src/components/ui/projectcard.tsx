2041712af82e0d5404d6f45cd6c6173b299c0183
plhnk <paul.hanaoka@gmail.com>
2024-05-15 14:22:42 -0700

    change label > labels, add tags

--- src/components/ui/projectcard.tsx
+++ src/components/ui/projectcard.tsx
@@ -6,21 +6,20 @@ import { useTheme } from 'next-themes';
 
 interface ProjectCardProps {
   id: string;
-  label: string;
+  labels: string[];
   title: string;
   subtitle: string;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
   id,
-  label,
+  labels,
   title,
   subtitle,
 }) => {
   const { theme, systemTheme } = useTheme();
   const [imageSrc, setImageSrc] = useState('');
   // <string>(`/projects/${id}/cover-${theme}.jpg`);
-  
 
   useEffect(() => {
     const currentTheme = theme === 'system' ? systemTheme : theme;
@@ -44,8 +43,19 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
         <div className="shadow" />
       </CardContent>
       <CardHeader className="px-0">
+        {labels && (
+          <div className="flex mb-2 -ml-0.5">
+            {labels.map((label, index) => (
+              <div
+                className="rounded-full text-nowrap mr-2 px-3 py-1 bg-card text-element text-xs uppercase tracking-wider"
+                key={index}
+              >
+                {label}
+              </div>
+            ))}
+          </div>
+        )}
         <CardTitle className="normal-case mb-2 tracking-normal *:block *:font-normal">
-          <span className="text-text/40 uppercase leading-loose">{label}</span>
           <span className="text-text text-base">{title}</span>
           <span className="text-text/60 text-base">{subtitle}</span>
         </CardTitle>
