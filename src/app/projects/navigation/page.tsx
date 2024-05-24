cf00f5adc1d1b7ecb7026ea1e641b9087150e79c
plhnk <paul.hanaoka@gmail.com>
2024-05-24 09:33:15 -0700

    update images

---
+++ src/app/projects/navigation/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+import MdxLayout from '../../../components/mdx-layout';
+import Content from './content.mdx';
+
+export default function Projects() {
+  return (
+    <MdxLayout caseStudy>
+      <Content />
+    </MdxLayout>
+  );
+}
