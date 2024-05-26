f3bccfbaa11d7caa658ce4ae50e0bbdee4fac885
plhnk <paul.hanaoka@gmail.com>
2024-05-25 23:30:19 -0700

    update resume section w/bullets

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -24,17 +24,17 @@ const WorkExperienceComponent: React.FC<Props> = ({ workExperience }) => {
               <h3 className="text-base font-normal m-0">
                 {experience.title} at {experience.company}
               </h3>
-              <p className="italic text-text/70">{experience.location}</p>
+              <p className="italic text-text/70 mb-1">{experience.location}</p>
             </div>
             <p>{experience.duration}</p>
           </div>
-          <p className="text-text/70 font-light mt-2 max-w-[58ch]">{experience.description}</p>
-          {/* <h4 className="text-base text-text/70 font-normal mt-8 mb-2">Responsibilities</h4>
+          <p className="text-text/70 italic font-light mt-0 max-w-[58ch]" dangerouslySetInnerHTML={{ __html: experience.description }}></p>
           <ul>
             {experience.responsibilities.map((responsibility, index) => (
-              <li key={index}>{responsibility}</li>
+              <li key={index} dangerouslySetInnerHTML={{ __html: responsibility }}></li>
             ))}
           </ul>
+          {/* <h4 className="text-base text-text/70 font-normal mt-8 mb-2">Responsibilities</h4>
           {experience.key_contributions && (
             <div>
               <h4 className="text-base text-text/70 font-normal mt-8 mb-2">Key Contributions:</h4>
