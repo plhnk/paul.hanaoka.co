2d8df63f963906334dc41e94287e94d6b9e7f7c5
plhnk <paul.hanaoka@gmail.com>
2024-06-03 16:36:36 -0700

    add H1 to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,14 @@ import Projects from '@/components/projects';
 export default function Home() {
   return (
     <>
-      {/* first div is the main content */}
-        <Projects className='2xl:col-start-2' />
-      {/* the 2nd div is the sidebar */}
-      <div className="sidebar">
+      <div className="defaultContainer row-span-1 md:max-lg:-mb-8">
+        <h1 className='text-base mb-0'>Designer &amp; Developer </h1>
+      </div>
+      <Projects className="2xl:col-start-2 row-span-2" />
+      <div className="sidebar row-span-3 lg:row-start-1">
         <Calendar className="col-span-2" />
         <Weather className="col-span-2" />
-        <Photos className='col-span-4 sm:col-span-2 h-[70vh] sm:max-h-96' />
+        <Photos className="col-span-4 sm:col-span-2 h-[70vh] sm:max-h-96" />
       </div>
     </>
   );
