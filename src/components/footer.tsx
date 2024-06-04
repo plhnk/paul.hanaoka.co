0c4cb3c98e91c0416361825962886c440b6963c7
plhnk <paul.hanaoka@gmail.com>
2024-06-04 15:38:23 -0700

    refine large display grid

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import WeirdGuy from './widgets/weird-guy';
 const Footer: React.FC = () => {
   return (
     <footer className="main-grid m-4 sm:mt-60 sm:m-8 md:mb-24 sm:ml-80 2xl:ml-8">
-      <div className="col-span-5 col-start-1 sm:col-start-2  after:absolute after:-top-4 after:-left-4 after:-right-4 after:-bottom-4 p-4 after:bg-transparent-window after:m-4 relative after:pointer-events-none">
+      <div className="col-span-5 col-start-1 lg:col-start-2  after:absolute after:-top-4 after:-left-4 after:-right-4 after:-bottom-4 p-4 after:bg-transparent-window after:m-4 relative after:pointer-events-none">
         <Proverbs />
       </div>
       {/* <WeirdGuy imageUrl="/images/tiny-dank-guy.png" /> */}
