0c3f57255be7d4a938f1f3aa3c709356c7e4e9cd
plhnk <paul.hanaoka@gmail.com>
2024-06-03 23:22:54 -0700

    fix flashing theme

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,9 +1,26 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+// import { useEffect } from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     window.requestAnimationFrame(() => {
+  //       // Trigger a reflow/repaint
+  //     });
+  //   };
+
+  //   window.addEventListener('scroll', handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
+  return (
+    <NextThemesProvider attribute="class" {...props}>
+      {children}
+    </NextThemesProvider>
+  );
 }
