0d549074f557aa20d6a1ba763208756b410f90b3
plhnk <paul.hanaoka@gmail.com>
2024-06-19 09:36:52 -0700

    add publish-ability

--- src/components/people.tsx
+++ src/components/people.tsx
@@ -4,12 +4,12 @@ import PeopleData from '@/lib/data/People.json';
 import { convertIdToFilename } from '@/lib/utils';
 import Link from '@/components/ui/link';
 
-// Define the Person interface to match the JSON structure
 interface Link {
   [key: string]: string;
 }
 
 interface Person {
+  publish: boolean;
   id: string;
   currentTitle: string;
   currentCompany: string;
@@ -19,54 +19,58 @@ interface Person {
   links: Link[];
 }
 
-// Explicitly assert the type of PeopleData
 const people: Person[] = PeopleData as Person[];
 
 const People: React.FC<{ className?: string }> = ({ className }) => {
   const sortedPeople = [...people].sort((a, b) => a.id.localeCompare(b.id));
 
+  const truncateText = (text: string, length: number) => {
+    return text.length > length ? text.substring(0, length) + '...' : text;
+  };
+
   return (
-    <ul className="grid col-span-9 grid-cols-3">
-      {sortedPeople.map((person) => (
-        <li key={person.id} className="col-span-1">
-          <div className="flex gap-4">
-            <div className="w-16 h-20 rounded-full overflow-hidden">
-              <img
-                className="w-full h-full object-cover"
-                src={`/headshots/${convertIdToFilename(person.id)}`}
-                alt={person.id}
-              />
-            </div>
-            <div className="flex flex-col gap-2 w-80 mt-4">
-              <h3 className="font-semibold">
-                {person.id}{' '}
-                <span>
-                  {person.relationship} {person.workedTogether}
-                </span>
-                <br />
-                <span>
-                  {person.currentTitle} @ {person.currentCompany}
-                </span>
-              </h3>
-              <p className="text-sm">{person.testimonial}</p>
+    <ul className="grid col-start-2 col-span-8 grid-cols-2 xl:grid-cols-3 gap-16 -ml-28">
+      {sortedPeople.map(
+        (person) =>
+          person.publish === true && (
+            <li key={person.id} className="col-span-1 ">
               <div className="flex gap-4">
-                {person.links.map((link, index) => {
-                  const [platform, url] = Object.entries(link)[0];
-                  return (
-                    <Link
-                      key={index}
-                      href={url}
-                      className='text-text/50'
-                    >
-                      {platform}
-                    </Link>
-                  );
-                })}
+                <div className="w-24 h-32 shrink-0 rounded-full overflow-hidden outline -outline-offset-1 outline-background/40">
+                  <img
+                    className="w-full h-full object-cover"
+                    src={`/headshots/${convertIdToFilename(person.id)}`}
+                    alt={person.id}
+                  />
+                </div>
+                <div className="flex flex-col gap-2 mt-8">
+                  <h3 className="font-semibold">
+                    {person.id}{' '}
+                    <span className="text-text/50">
+                      <span className="">
+                        {person.relationship} {person.workedTogether}
+                      </span>
+                      <br />
+                      <span className="font-light">
+                        {person.currentTitle} @ {person.currentCompany}
+                      </span>
+                    </span>
+                  </h3>
+                  <p className="text-text/80">{person.testimonial}</p>
+                  <div className="flex gap-4">
+                    {person.links.map((link, index) => {
+                      const [platform, url] = Object.entries(link)[0];
+                      return (
+                        <Link key={index} href={url} className="text-text/50">
+                          {platform}
+                        </Link>
+                      );
+                    })}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </li>
-      ))}
+            </li>
+          )
+      )}
     </ul>
   );
 };
