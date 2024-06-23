2e6ada21781589b484b8e38089e5f174a65f6a20
plhnk <paul.hanaoka@gmail.com>
2024-06-22 17:42:28 -0700

    fix layout bug caused by people grid

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import WeirdGuy from './widgets/weird-guy';
 const Footer: React.FC = () => {
   return (
     <footer className="main-grid m-4 sm:mt-60 sm:m-8 md:mb-24 sm:ml-80 xl:ml-0">
-      <div className="col-span-5 col-start-1 lg:col-start-2  side-scroll-blender">
+      <div className="col-span-3 md:col-span-5 col-start-1 lg:col-start-2  side-scroll-blender">
         <Proverbs />
       </div>
       {/* <WeirdGuy imageUrl="/images/tiny-dank-guy.png" /> */}
