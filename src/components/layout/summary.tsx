80a37e39046353b1c6c459d7278dbb1418aaad48
plhnk <paul.hanaoka@gmail.com>
2024-07-18 13:08:56 -0700

    add device, svgs

--- src/components/layout/summary.tsx
+++ src/components/layout/summary.tsx
@@ -19,21 +19,21 @@ const Summary: React.FC<SummaryProps> = ({
         </h3>
         <p className="my-[.5em] ">{problem}</p>
       </div>
-      <div className="flex gap-4 sm:gap-8 col-span-3 md:max-lg:col-span-4">
-        <div className="">
+      <div className="grid grid-cols-subgrid gap-4 sm:gap-8 col-span-3 md:col-span-5">
+        <div className="col-span-3">
           <h3 className="text-element uppercase tracking-widest text-xs">
             Role
           </h3>
           <p className="my-[.5em] ">{role}</p>
         </div>
-        <div className="min-w-fit">
+        <div className="col-span-3 md:col-span-2">
           <h3 className="text-element uppercase tracking-widest text-xs">
             Contributions
           </h3>
-          <ul className="py-2">
+          <ul className="py-2 flex flex-wrap">
             {contributions.map((item, index) => (
               <li
-                className='leading-6 px-0 mb-2 before:content-["•"] before:absolute before:text-element/50 before:-ml-2.5'
+                className='flex-none w-1/2 lg:w-full min-w-[12ch] max-w-[20ch] leading-6 px-0 mb-2 before:content-["•"] before:absolute before:text-element/50 before:-ml-2.5'
                 key={index}
               >
                 {item}
---
+++ src/components/svg/user.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+
+interface HammerProps {
+  className?: string;
+}
+
+const Hammer: React.FC<HammerProps> = ({ className }) => {
+
+  return (
+    <div className={cn('', className)}>
+      <svg
+        width="200"
+        height="200"
+        viewBox="0 0 200 200"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M139 196V157.667C139 147.412 134.876 137.577 127.535 130.325C120.195 123.074 110.238 119 99.8571 119H41.1429C30.7615 119 20.8054 123.074 13.4647 130.325C6.12397 137.577 2 147.412 2 157.667L2 196"
+          stroke="white"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeDasharray="8 16"
+        />
+        <path
+          d="M70.5 80C92.3153 80 110 62.5391 110 41C110 19.4609 92.3153 2 70.5 2C48.6848 2 31 19.4609 31 41C31 62.5391 48.6848 80 70.5 80Z"
+          stroke="white"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeDasharray="8 16"
+        />
+        <path
+          d="M168 51V109"
+          stroke="white"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeDasharray="8 16"
+        />
+        <path
+          d="M197 80H139"
+          stroke="white"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeDasharray="8 16"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default Hammer;
