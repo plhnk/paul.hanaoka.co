63198a5ee9ee6cd0ddcece1c05560a9e003f12c7
plhnk <paul.hanaoka@gmail.com>
2024-07-23 14:51:28 -0700

    polish slider variant

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -67,3 +67,18 @@ export function yearsSince(year: number): number {
 
   return years;
 }
+
+export function throttle<T extends (...args: any[]) => void>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle: boolean;
+  return function(this: any, ...args: Parameters<T>) {
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
