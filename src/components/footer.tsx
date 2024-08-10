15d30c8d8b9b9b10e14c97fe4b7176a3655969b9
plhnk <paul.hanaoka@gmail.com>
2024-08-09 23:54:12 -0700

    draft updated Footer for desktop

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Proverbs from './widgets/proverbs';
 import Weather from './widgets/weather';
 import Photos from './widgets/photos';
 import Calendar from './widgets/calendar';
+import { getDateInfo } from '../lib/utils';
+import Link from '@/components/ui/link';
+import { MousePointer2, MoveUp } from 'lucide-react';
 
 const Footer: React.FC = () => {
+  const year = getDateInfo().year;
+  const [isVisible, setIsVisible] = useState(false);
+  const [atBottom, setAtBottom] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      const windowHeight = window.innerHeight;
+      const fullHeight = document.documentElement.scrollHeight;
+
+      setIsVisible(scrollTop > 300);
+      setAtBottom(scrollTop + windowHeight >= fullHeight - 10);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  const toTop = (
+    <button
+      onClick={scrollToTop}
+      className={`group fixed bottom-11 right-8 py-3 px-2 rounded-full shadow-lg transition-all duration-300 ease-in-out ${
+        isVisible ? 'opacity-100' : 'opacity-0'
+      } ${atBottom ? 'bg-accent' : 'bg-element/20'} text-white/80`}
+      aria-label="Scroll to top"
+    >
+      <div className="relative w-5 h-8">
+        <MoveUp className="w-5 h-8 absolute top-0 left-0 transform transition-all duration-300 group-hover:-translate-y-6 group-hover:opacity-0 ease-in-out" />
+        <MoveUp className="w-5 h-8 absolute top-0 left-0 transform transition-all duration-300 group-hover:translate-y-0 translate-y-6 opacity-0 group-hover:opacity-100 ease-in-out" />
+      </div>
+    </button>
+  );
+
   return (
-    <footer className="main-grid m-4 sm:mt-48 sm:pb-48 sm:m-8 sm:ml-80 xl:ml-0">
-      <Weather className='col-start-2 col-span-2' />
-      <Calendar className='col-span-2' />
-      <Photos className='col-span-3 row-span-2' />
-      <Proverbs className='col-start-2 col-span-4' />
+    <footer className="main-grid m-4 sm:mt-48 sm:m-8 sm:ml-80 xl:ml-0">
+      <Weather className="lg:col-start-2 col-span-2" />
+      <Calendar className="col-span-2" />
+      <Photos className="col-span-3 row-span-2" />
+      <Proverbs className="lg:col-start-2 col-span-4" />
+      <div className="flex justify-between lg:col-start-2 col-span-7 mt-40 mb-14 text-text/80">
+        <div className="">
+          <MousePointer2 className="-ml-8 -mt-8 inline mr-2 text-text/20" />
+          <Link href="/about#Colophon">Designed & Built in the PNW </Link>
+        </div>
+        <Link href="/privacy">Privacy</Link>
+        <span className="">{year + ' ©️ plhnk'}</span>
+      </div>
+      {toTop}
     </footer>
   );
 };
