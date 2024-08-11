5ea563b6f3935d1e5ad2f1bb6b850b493820d15c
plhnk <paul.hanaoka@gmail.com>
2024-08-10 21:40:09 -0700

    try to solve nextjs image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
-import remarkGfm from 'remark-gfm'
-import createMDX from '@next/mdx'
- 
+import remarkGfm from 'remark-gfm';
+import createMDX from '@next/mdx';
+
+// Prevent images from being optimized in preview or development
+const isProduction = process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
   images: {
+    unoptimized: !isProduction, // Disables image optimization unless it's production
     remotePatterns: [
       {
         protocol: 'https',
@@ -18,15 +22,15 @@ const nextConfig = {
       },
     ],
   },
-}
- 
+};
+
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [],
   },
-})
- 
+});
+
 // Wrap MDX and Next.js config with each other
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
