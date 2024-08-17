70264a21d8334a1f6889c12a768bbca9a4c663ca
plhnk <paul.hanaoka@gmail.com>
2024-08-17 14:57:44 -0700

    update post page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,20 +1,24 @@
 import Link from 'next/link';
 import { getAllPosts } from '@/server/api/posts';
 
+function parseDate(dateString: string): Date {
+  const date = new Date(dateString);
+  return isNaN(date.getTime()) ? new Date(0) : date; // Default to epoch if invalid
+}
+
 export default async function BlogIndex() {
   const posts = await getAllPosts();
 
+  // Sort posts by date, newest first
+  posts.sort((a, b) => parseDate(b.frontmatter.date).getTime() - parseDate(a.frontmatter.date).getTime());
+
   return (
-    <ul>
+    <ul className='col-start-3 col-span-3'>
       {posts.map((post) => (
-        <li key={post.slug} className="my-16">
+        <li key={post.slug} className="my-16 flex flex-col gap-4">
           <Link href={`/posts/${post.slug}`}>
             {post.frontmatter.title || 'Untitled Post'}
           </Link>
-          {post.frontmatter.date && <span> - {post.frontmatter.date}</span>}
-          {post.frontmatter.author && (
-            <span> by {post.frontmatter.author}</span>
-          )}
         </li>
       ))}
     </ul>
