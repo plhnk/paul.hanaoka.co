39b31cde2b3a44dba83b90189f8ee17fd6c38362
plhnk <paul.hanaoka@gmail.com>
2024-08-21 20:09:35 -0700

    polish

--- src/components/widgets/availability.tsx
+++ src/components/widgets/availability.tsx
@@ -62,31 +62,31 @@ export default function StatusIndicator() {
         'bg-orange-950/50 text-orange-200 outline-orange-950/30';
       innerDivClasses = 'bg-orange-500 outline-orange-700/50';
       break;
-    case 'sleeping':
+    case 'offline':
+      outerSpanClasses = 'bg-blue-950/50 text-blue-200 outline-blue-950/30';
+      innerDivClasses = 'bg-blue-500 outline-blue-700/50';
+      break;
+    case 'error':
       outerSpanClasses = 'bg-red-950/50 text-red-200 outline-red-950/30';
       innerDivClasses = 'bg-red-500 outline-red-700/50';
       break;
-    case 'error':
+    default:
       outerSpanClasses = 'bg-gray-950/50 text-gray-200 outline-gray-950/30';
       innerDivClasses = 'bg-gray-500 outline-gray-700/50';
-      break;
-    default:
-      outerSpanClasses = 'bg-blue-950/50 text-blue-200 outline-blue-950/30';
-      innerDivClasses = 'bg-blue-500 outline-blue-700/50';
       break;
   }
 
   return (
-    <HoverCard>
+    <HoverCard openDelay={100} closeDelay={300}>
       <HoverCardTrigger>
-        <span
-          className={`mt-6 flex items-center gap-2 small-caps font-mono font-light text-xs px-2.5 py-1 rounded-full outline outline-2 -outline-offset-1 ${outerSpanClasses}`}
+        <div
+          className={`cursor-pointer md:mt-4 lg:mt-6 flex items-center gap-2 small-caps font-mono font-light text-xs px-2.5 py-1 rounded-full outline outline-2 -outline-offset-1 ${outerSpanClasses}`}
         >
-          <div
+          <span
             className={`animate-pulse rounded-full h-2 w-2 outline outline-2 -outline-offset-1 ${innerDivClasses}`}
           />{' '}
           {status}
-        </span>
+        </div>
       </HoverCardTrigger>
       <HoverCardContent
         align="end"
@@ -110,7 +110,6 @@ export default function StatusIndicator() {
                 hotkey={'c'}
                 url={'https://cal.com/plhnk'}
               />
-
               <NavButton
                 minimal
                 icon={<Mail {...iconStyle} />}
