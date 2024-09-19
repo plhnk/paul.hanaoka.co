e50ac43237bbca8af98ed937fc5db030cd77833a
plhnk <paul.hanaoka@gmail.com>
2024-09-19 00:21:58 -0700

    remove dynamic import for mdx client

--- src/components/client-post-component.tsx
+++ src/components/client-post-component.tsx
@@ -1,14 +1,16 @@
-'use client'
+'use client';
 
-import dynamic from 'next/dynamic'
-import MDXImage from '@/components/mdx-image'
+import { MDXRemote } from 'next-mdx-remote'; // Static import
+import MDXImage from '@/components/mdx-image';
 
-const MDXRemote: any = dynamic(() => import('next-mdx-remote').then(mod => mod.MDXRemote), { ssr: false })
+// import dynamic from 'next/dynamic'
+
+// const MDXRemote: any = dynamic(() => import('next-mdx-remote').then(mod => mod.MDXRemote), { ssr: false })
 
 const components = {
   img: MDXImage,
-}
+};
 
 export default function ClientPostContent({ content }: { content: any }) {
-  return <MDXRemote {...content} components={components} />
+  return <MDXRemote {...content} components={components} />;
 }
